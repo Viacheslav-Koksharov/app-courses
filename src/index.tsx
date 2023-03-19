e16b87ffecfdf735b8135523d10a0863a4ca5364
Viacheslav Koksharov <v.koksharov82@gmail.com>
2023-03-20 00:33:37 +0200

    fixes index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import { LessonProvider } from './context/LessonContextProvider';
 import App from './components/App/App';
 import 'modern-normalize/modern-normalize.css';
@@ -8,13 +8,16 @@ import './index.css';
 
 const rootElement = document.getElementById('root') as HTMLElement;
 const root = createRoot(rootElement);
-
+const router = createHashRouter([
+  {
+    path: '/*',
+    element: <App />,
+  },
+]);
 root.render(
   <StrictMode>
     <LessonProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </LessonProvider>
   </StrictMode>,
 );
